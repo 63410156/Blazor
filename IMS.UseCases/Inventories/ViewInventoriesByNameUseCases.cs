781093bf2cafc89e1b9d81652069fcd527de01bb
63410156 <63410156@go.buu.ac.th>
2024-01-29 12:27:46 +0700

    Add project files.

---
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCases.cs
@@ -0,0 +1,28 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.Inventories
+{
+    // use outside inventoryRepository that is not instanceate in the class
+    public class ViewInventoriesByNameUseCase : IViewInventoriesByNameUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+
+        // construction also require the instance of inventoryRepository abstraction
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository) // plug in by InventoryRepository plugin
+        {
+            this.inventoryRepository = inventoryRepository;
+        }
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            // return a list of Inventory by name parameter which default value is ""
+            // pass through inventories
+            return await inventoryRepository.GetInventoriesByNameAsync(name);
+        }
+    }
+}
